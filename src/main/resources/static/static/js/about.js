55725aa054b18902660463aeb1336584a69e4c4a
xh <457071935@qq.com>
2018-07-18 17:16:01 +0800

    基本信息

--- src/main/resources/static/static/js/about.js
+++ src/main/resources/static/static/js/about.js
@@ -1,3 +1,21 @@
 /**
  * Created by Administrator on 2018/7/18.
  */
+$(function () {
+    var _this = this;
+    this.init = function() {
+        this.registerEvent();
+    };
+    this.registerEvent = function () {
+        $(".info").on("click",function () {
+            var $this = $(this);
+            var index =  $this.index();
+            $(".info").removeClass("active");
+            $this.addClass("active");
+            $(".content").hide();
+            $(".content").eq(index).show();
+        })
+    };
+
+    this.init();
+});
